e76b3cc6db651f59e78cacf4e444d0252873427a
risako070310 <risako070310@gmail.com>
2022-04-23 01:51:59 +0000

    add edit and delete

--- app.rb
+++ app.rb
@@ -28,3 +28,22 @@ get '/category/:id' do
     @items = @category.items
     erb :index
 end
+
+post '/delete/:id' do
+    Item.find(params[:id]).destroy
+    redirect '/'
+end
+
+get '/edit/:id' do
+    @item = Item.find(params[:id])
+    erb :edit
+end
+
+post '/renew/:id' do
+    @item = Item.find(params[:id])
+    @item.update({
+        title: params[:title],
+        price: params[:price]
+    })
+    redirect '/'
+end
