8ba15f51ca9987f4cf838b30489a1a183a0b7874
risako070310 <risako070310@gmail.com>
2022-04-23 00:26:13 +0000

    add category

--- app.rb
+++ app.rb
@@ -7,14 +7,24 @@ require './models/item.rb'
 get '/' do
     @items = Item.all
     @total = Item.sum(:price)
+    @categories = Category.all
     erb :index
 end
 
 post '/create' do
     Item.create({
         title: params[:title],
-        price: params[:price]
+        price: params[:price],
+        category_id: params[:category]
     })
     
     redirect '/'
+end
+
+get '/category/:id' do
+    @categories = Category.all
+    @category = Category.find(params[:id])
+    @category_name = @category.name
+    @items = @category.items
+    erb :index
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_080634) do
+ActiveRecord::Schema.define(version: 2022_04_22_090450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
